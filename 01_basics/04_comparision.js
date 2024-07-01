0b202cc4db9a3fdf21850beaa10061320c39e916
UjjawalMishra93 <themishraji93@gmail.com>
2024-07-01 10:57:07 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 = 1);
+// console.log(2 != 1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+//*NOTE: avoid above type of conversion for clean code.. it may be confuse 
+
+// ===
+
+console.log("2"===2);
