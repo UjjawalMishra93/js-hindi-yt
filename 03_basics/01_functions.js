1ec2c69d4ec34ac8efd02a8000785db034ce3945
UjjawalMishra93 <themishraji93@gmail.com>
2024-07-07 06:27:21 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,9 +1,9 @@
-console.log("H");
-console.log("I");
-console.log("J");
-console.log("K");
-console.log("L");
-console.log("M");
+// console.log("H");
+// console.log("I");
+// console.log("J");
+// console.log("K");
+// console.log("L");
+// console.log("M");
 
 function sayMyName(){
     console.log("H");
@@ -42,5 +42,37 @@ function loginUsermessage(username = "Sam"){
 }
 
 //console.log(loginUsermessage("Ujjawal"));
-console.log(loginUsermessage());
+// console.log(loginUsermessage());
 
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+
+}
+
+// console.log(calculateCartPrice(200, 400 , 500 , 1000));
+
+const user = {
+    username : "Ujjawal",
+    prices:"199"
+
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user);
+handleObject({
+    username:"sam",
+    price:"999"
+})
+
+
+const myNewArray = [200, 400, 100 , 600 ]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 100 , 600]));
