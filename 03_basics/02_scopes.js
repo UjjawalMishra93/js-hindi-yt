1c50700193318f9d23da27592193dca5c875f433
UjjawalMishra93 <themishraji93@gmail.com>
2024-07-08 11:47:17 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,12 +3,58 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    console.log("inner:", a);
+    //console.log("inner:", a);
     
 }
 
 
 
-console.log(a);
+//console.log(a);
 // console.log(b);
 //console.log(c);
+
+
+function one(){
+    const username = "Ujjawal"
+
+    function two(){
+        const website  = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    //two()
+}
+
+//one()
+
+if (true) {
+    const username = "Ujjawal"
+    if(username==="Ujjawal"){
+        const website = " youtube"
+        //console.log(username+website);
+
+    }
+   // console.log(website);
+    
+}
+ //console.log(username);
+
+
+ // ++++++++++++++ interestong +++++++++++++++++++++
+
+ console.log(addone(5));
+ function addone(num){
+    return num + 1
+
+
+ }
+
+
+ console.log(addTwo(5));
+ const addTwo = function(num){
+    return num + 2
+
+ }
+
+ 
