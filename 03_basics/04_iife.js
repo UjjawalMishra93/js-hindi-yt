edc96ddea8d9a44c0946a0732e426243daf9d6e1
UjjawalMishra93 <themishraji93@gmail.com>
2024-07-10 11:17:39 +0000

    control flow

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,10 +1,12 @@
 // Immediately Invoked Function Expressions (IIFE)
 
 (function chai(){
+    // named IIFE
     console.log(`DB CONNECTED`);
 })();
 
 
 (   (name) => {
+    // unname IIFE
     console.log(`DB CONNECTED TWO ${name}`);
 } )("Ujjawal")
